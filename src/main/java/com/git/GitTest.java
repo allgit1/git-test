25fb8d0684fb7203c8b9ebfeaa3e0a16d9258799
allgit1 <77422654+allgit1@users.noreply.github.com>
2022-05-25 21:18:39 +0800

    pull commit 2

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -14,6 +14,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("master test");
         System.out.println("push test");
-        System.out.pritln("hello idea");
+        System.out.println("hello idea");
     }
 }
