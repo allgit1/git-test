72cf5cd4c5c4885ac5cd688aa927515c938e21c4
mjh <navi_mjh@163.com>
2022-05-25 20:38:17 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.git;
+
+/**
+ * encoding='utf-8'
+ * author:mjh
+ * date:2022-05-25 20:35
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
