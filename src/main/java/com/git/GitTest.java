86b99dbe8497ed0515e619ce84a2bdf4e2a6e2de
mjh <navi_mjh@163.com>
2022-05-25 21:37:25 +0800

    push commit1

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("push test");
         System.out.println("hello idea");
+        System.out.println("hello github");
     }
 }
